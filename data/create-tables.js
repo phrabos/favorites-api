55139f8e58dc5292497db5329f0e5a41e34a656f
patrick hrabos <phrabos@gmail.com>
2021-03-03 19:04:14 -0500

    load seed data of NASA images

--- data/create-tables.js
+++ data/create-tables.js
@@ -17,10 +17,13 @@ async function run() {
                     email VARCHAR(256) NOT NULL,
                     hash VARCHAR(512) NOT NULL
                 );           
-                CREATE TABLE animals (
+                CREATE TABLE favorites (
                     id SERIAL PRIMARY KEY NOT NULL,
-                    name VARCHAR(512) NOT NULL,
-                    cool_factor INTEGER NOT NULL,
+                    date VARCHAR(512) NOT NULL,
+                    explanation VARCHAR(1500) NOT NULL,
+                    media_type VARCHAR(512) NOT NULL,
+                    title VARCHAR(512) NOT NULL,
+                    url VARCHAR(512) NOT NULL,
                     owner_id INTEGER NOT NULL REFERENCES users(id)
             );
         `);
